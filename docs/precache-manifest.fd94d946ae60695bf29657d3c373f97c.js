8a7f1990c8cbfb988ab3ee43a6c25f8fe220853f
John Andrés <johnandrescv@gmail.com>
2020-07-27 21:43:20 -0500

    Agregamos los docs

---
+++ docs/precache-manifest.fd94d946ae60695bf29657d3c373f97c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209de7fb9b5de13c4a137deecf79759d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72080c1fdc50c1ab5a2d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cf5d1f74a89d38607960",
+    "url": "/static/js/2.bacb539b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bacb539b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72080c1fdc50c1ab5a2d",
+    "url": "/static/js/main.bb664a48.chunk.js"
+  },
+  {
+    "revision": "0f7572d0a9713a442582",
+    "url": "/static/js/runtime-main.4e50ff5b.js"
+  }
+]);
